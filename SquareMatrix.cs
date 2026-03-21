95aca1641a7837cc17972baf1fa572d4917a32a4
v01d <Kevin_Vearez@mail.ru>
2026-03-21 23:51:04 +0700

    Add all overloads. Add new methods.

--- SquareMatrix.cs
+++ SquareMatrix.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -60,6 +62,214 @@ namespace lab3 {
       }
 
       return resultMatrix;
+    }
+
+    // Overloading the multiplication operator for multiplying two matrices
+    public static SquareMatrix operator *(SquareMatrix firstMatrix, SquareMatrix secondMatrix) {
+      // Checking the sizes of the matrices
+      if (firstMatrix.size != secondMatrix.size) {
+        throw new ArgumentException("The sizes of the matrices must be equal");
+      }
+
+      // Result of the multiplication
+      SquareMatrix resultMatrix = new SquareMatrix(firstMatrix.size);
+
+      // Multiplying the matrices
+      for (int indexOfRow = 0; indexOfRow < firstMatrix.size; ++indexOfRow) {
+
+        // Iterating through the columns of the second matrix
+        for (int indexOfColumn = 0; indexOfColumn < firstMatrix.size; ++indexOfColumn) {
+          int sumOfTheProducts = 0;
+          for (int indexOfElement = 0; indexOfElement < firstMatrix.size; ++indexOfElement) {
+            sumOfTheProducts += firstMatrix.data[indexOfRow, indexOfElement] * secondMatrix.data[indexOfElement, indexOfColumn];
+          }
+
+          // Storing the result
+          resultMatrix.data[indexOfRow, indexOfColumn] = sumOfTheProducts;
+        }
+      }
+
+      return resultMatrix;
+    }
+
+    // Overload for operator <
+    public static SquareMatrix operator <(SquareMatrix firstMatrix, SquareMatrix secondMatrix) {
+      // Checking the sizes of the matrices
+      if (firstMatrix.size != secondMatrix.size) {
+        throw new ArgumentException("The sizes of the matrices must be equal");
+      }
+
+      // Result of the comparison
+      SquareMatrix resultMatrix = new SquareMatrix(firstMatrix.size);
+
+      // Comparing the matrices
+      for (int indexOfRow = 0; indexOfRow < firstMatrix.size; ++indexOfRow) {
+        for (int indexOfColumn = 0; indexOfColumn < firstMatrix.size; ++indexOfColumn) {
+          resultMatrix.data[indexOfRow, indexOfColumn] = firstMatrix.data[indexOfRow, indexOfColumn] < secondMatrix.data[indexOfRow, indexOfColumn] ? 1 : 0;
+        }
+      }
+
+      return resultMatrix;
+    }
+
+    // Overload for operator >
+    public static SquareMatrix operator >(SquareMatrix firstMatrix, SquareMatrix secondMatrix) {
+      // Checking the sizes of the matrices
+      if (firstMatrix.size != secondMatrix.size) {
+        throw new ArgumentException("The sizes of the matrices must be equal");
+      }
+
+      // Result of the comparison
+      SquareMatrix resultMatrix = new SquareMatrix(firstMatrix.size);
+
+      // Comparing the matrices
+      for (int indexOfRow = 0; indexOfRow < firstMatrix.size; ++indexOfRow) {
+        for (int indexOfColumn = 0; indexOfColumn < firstMatrix.size; ++indexOfColumn) {
+          resultMatrix.data[indexOfRow, indexOfColumn] = firstMatrix.data[indexOfRow, indexOfColumn] > secondMatrix.data[indexOfRow, indexOfColumn] ? 1 : 0;
+        }
+      }
+
+      return resultMatrix;
+    }
+
+    // >=
+    public static SquareMatrix operator >=(SquareMatrix firstMatrix, SquareMatrix secondMatrix) {
+      // Checking the sizes of the matrices
+      if (firstMatrix.size != secondMatrix.size) {
+        throw new ArgumentException("The sizes of the matrices must be equal");
+      }
+
+      // Result of the comparison
+      SquareMatrix resultMatrix = new SquareMatrix(firstMatrix.size);
+
+      // Comparing the matrices
+      for (int indexOfRow = 0; indexOfRow < firstMatrix.size; ++indexOfRow) {
+        for (int indexOfColumn = 0; indexOfColumn < firstMatrix.size; ++indexOfColumn) {
+          resultMatrix.data[indexOfRow, indexOfColumn] = firstMatrix.data[indexOfRow, indexOfColumn] >= secondMatrix.data[indexOfRow, indexOfColumn] ? 1 : 0;
+        }
+      }
+
+      return resultMatrix;
+    }
+
+    // <=
+    public static SquareMatrix operator <=(SquareMatrix firstMatrix, SquareMatrix secondMatrix) {
+      // Check
+      if (firstMatrix.size != secondMatrix.size) {
+        throw new ArgumentException("The sizes of the matrices must be equal");
+      }
+
+      // Result of the comparison
+      SquareMatrix resultMatrix = new SquareMatrix(firstMatrix.size);
+
+      // Comparing the matrices
+      for (int indexOfRow = 0; indexOfRow < firstMatrix.size; ++indexOfRow) {
+        for (int indexOfColumn = 0; indexOfColumn < firstMatrix.size; ++indexOfColumn) {
+          resultMatrix.data[indexOfRow, indexOfColumn] = firstMatrix.data[indexOfRow, indexOfColumn] <= secondMatrix.data[indexOfRow, indexOfColumn] ? 1 : 0;
+        }
+      }
+
+      return resultMatrix;
+    }
+
+    // ==
+    public static SquareMatrix operator ==(SquareMatrix firstMatrix, SquareMatrix secondMatrix) {
+      // Checking the sizes of the matrices
+      if (firstMatrix.size != secondMatrix.size) {
+        throw new ArgumentException("The sizes of the matrices must be equal");
+      }
+
+      // Result of the comparison
+      SquareMatrix resultMatrix = new SquareMatrix(firstMatrix.size);
+
+      // Comparing the matrices
+      for (int indexOfRow = 0; indexOfRow < firstMatrix.size; ++indexOfRow) {
+        for (int indexOfColumn = 0; indexOfColumn < firstMatrix.size; ++indexOfColumn) {
+          resultMatrix.data[indexOfRow, indexOfColumn] = firstMatrix.data[indexOfRow, indexOfColumn] == secondMatrix.data[indexOfRow, indexOfColumn] ? 1 : 0;
+        }
+      }
+
+      return resultMatrix;
+    }
+
+    // !=
+    public static SquareMatrix operator !=(SquareMatrix firstMatrix, SquareMatrix secondMatrix) {
+      // Checking the sizes of the matrices
+      if (firstMatrix.size != secondMatrix.size) {
+        throw new ArgumentException("The sizes of the matrices must be equal");
+      }
+
+      // Result of the comparison
+      SquareMatrix resultMatrix = new SquareMatrix(firstMatrix.size);
+
+      // Comparing the matrices
+      for (int indexOfRow = 0; indexOfRow < firstMatrix.size; ++indexOfRow) {
+        for (int indexOfColumn = 0; indexOfColumn < firstMatrix.size; ++indexOfColumn) {
+          resultMatrix.data[indexOfRow, indexOfColumn] = firstMatrix.data[indexOfRow, indexOfColumn] != secondMatrix.data[indexOfRow, indexOfColumn] ? 1 : 0;
+        }
+      }
+
+      return resultMatrix;
+    }
+
+    // Overriding the ToString method for displaying the matrix
+    public override string ToString() {
+      // Using StringBuilder for efficient string concatenation
+      StringBuilder stringBuilder = new StringBuilder();
+
+      // Iterating through the matrix and appending its elements to the StringBuilder
+      for (int indexOfRow = 0; indexOfRow < size; ++indexOfRow) {
+        for (int indexOfColumn = 0; indexOfColumn < size; ++indexOfColumn) {
+          stringBuilder.Append(data[indexOfRow, indexOfColumn] + " ");
+        }
+
+        stringBuilder.Append(Environment.NewLine);
+      }
+
+      // Returning the string representation of the matrix
+      return stringBuilder.ToString();
+    }
+
+    public override bool Equals(object obj) {
+      // Checking if the object is null or if it is of a different type
+      if (obj == null || GetType() != obj.GetType()) {
+        return false;
+      }
+
+      // Casting the object to SquareMatrix
+      SquareMatrix otherMatrix = (SquareMatrix)obj;
+
+      // Checking the sizes of the matrices
+      if (size != otherMatrix.size) {
+        return false;
+      }
+
+      // Comparing the matrices element by element
+      for (int indexOfRow = 0; indexOfRow < size; ++indexOfRow) {
+        for (int indexOfColumn = 0; indexOfColumn < size; ++indexOfColumn) {
+          // If any element is different, the matrices are not equal
+          if (data[indexOfRow, indexOfColumn] != otherMatrix.data[indexOfRow, indexOfColumn]) {
+            return false;
+          }
+        }
+      }
+
+      return true;
+    }
+
+    // Overriding the GetHashCode method for generating a hash code for the matrix
+    public override int GetHashCode() {
+      // Starting with a non-zero prime number
+      int hash = 17;
+
+      // Combining the hash codes of the size and the elements of the matrix
+      for (int indexOfRow = 0; indexOfRow < size; ++indexOfRow) {
+        for (int indexOfColumn = 0; indexOfColumn < size; ++indexOfColumn) {
+          hash = hash * 31 + data[indexOfRow, indexOfColumn].GetHashCode();
+        }
+      }
+
+      return hash;
     }
   }
 }
