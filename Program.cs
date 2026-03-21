4de13516c65b38e726d1b5c4c4469e0b60282fd4
v01d <Kevin_Vearez@mail.ru>
2026-03-22 00:50:40 +0700

    Menu is done. Lab is ready for review.

--- Program.cs
+++ Program.cs
@@ -5,8 +5,41 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace lab3 {
-    internal class Program {
-      static void Main(string[] args) {
+  internal class Program {
+    static void Main(string[] args) {
+      try {
+        // Init two matrices with size 3x3
+        SquareMatrix A = new SquareMatrix(3, 0, 5);
+        SquareMatrix B = new SquareMatrix(3, 0, 5);
+
+        Console.WriteLine($"Matrix A:\n{A}");
+
+        Console.WriteLine($"Matrix B:\n{B}");
+
+        // Addition
+        SquareMatrix sum = A + B;
+        Console.WriteLine($"A + B:\n{sum}");
+
+        // Multiplication
+        SquareMatrix product = A * B;
+        Console.WriteLine($"A * B:\n{product}");
+
+        // Equality check
+        Console.WriteLine("A == B?");
+        Console.WriteLine(A == B);
+
+        // Cloning
+        SquareMatrix cloneA = A.Clone();
+        Console.WriteLine($"Clone A:\n{cloneA}");
+
+        // Exception check
+        SquareMatrix C = new SquareMatrix(2);
+        Console.WriteLine("try A + C: ");
+        // Trigger
+        SquareMatrix fail = A + C;
+      } catch (MatrixSizeException sizeException) {
+        Console.WriteLine("Exception cought: " + sizeException.Message);
       }
     }
+  }
 }
