da9a4745ee693cf39931954707094c451c861659
v01d <Kevin_Vearez@mail.ru>
2026-03-21 22:05:45 +0700

    Architectural Changes
    The logic for creating matrices has been moved to a separate file

---
+++ SquareMatrix.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab3 {
+  // Class for generating a square matrixes
+  public class SquareMatrix {
+    // Declaring a two-dimensional array for storing the matrix data and a variable for storing the size of the matrix
+    private int[,] data;
+    private int size;
+    private static Random randomObjectForFilling = new Random();
+
+    // Constructor for filling the matrix with random numbers in the range from 0 to 9
+    public SquareMatrix(int sizeOfMatrix) {
+      this.size = sizeOfMatrix;
+
+      // Random data
+      data = new int[sizeOfMatrix, sizeOfMatrix];
+
+      // Filling
+      for (int indexOfRow = 0; indexOfRow < sizeOfMatrix; ++indexOfRow) {
+        for (int indexOfColumn = 0; indexOfColumn < sizeOfMatrix; ++indexOfColumn) {
+          data[indexOfRow, indexOfColumn] = randomObjectForFilling.Next(0, 10);
+        }
+      }
+    }
+
+    // Constructor with parameters for filling the matrix with random numbers in a specified range
+    public SquareMatrix(int sizeOfMatrix, int min, int max) {
+      this.size = sizeOfMatrix;
+
+      // Random data
+      data = new int[sizeOfMatrix, sizeOfMatrix];
+
+      // Filling
+      for (int indexOfRow = 0; indexOfRow < sizeOfMatrix; ++indexOfRow) {
+        for (int indexOfColumn = 0; indexOfColumn < sizeOfMatrix; ++indexOfColumn) {
+          data[indexOfRow, indexOfColumn] = randomObjectForFilling.Next(min, max);
+        }
+      }
+    }
+  }
+}
